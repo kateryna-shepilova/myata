62816a945b95568cfa1d0bc5619286026022dae8
sarpensortia <kateryna.shepilova@gmail.com>
2022-02-03 01:04:19 +0200

    Added desktop layout

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+	$('.single-item').slick({
+		infinite: true,
+		dots: true
+	});
+});
+
+function bootstrapTabControl() {
+	var i, items = $('.btn-tabs'), pane = $('.tab-pane');
+
+	$('.nexttab').on('click', function () {
+		for (i = 0; i < items.length; i++) {
+			if ($(items[i]).hasClass('active') == true) {
+				break;
+			}
+		}
+
+		if (i < items.length - 1) {
+			$(items[i]).removeClass('active');
+			$(items[i + 1]).addClass('active');
+			$(pane[i]).removeClass('show active');
+			$(pane[i + 1]).addClass('show active');
+		}
+	});
+
+	$('.prevtab').on('click', function () {
+		for (i = 0; i < items.length; i++) {
+			if ($(items[i]).hasClass('active') == true) {
+				break;
+			}
+		}
+		if (i != 0) {
+			$(items[i]).removeClass('active');
+			$(items[i - 1]).addClass('active');
+			$(pane[i]).removeClass('show active');
+			$(pane[i - 1]).addClass('show active');
+		}
+	});
+}
+bootstrapTabControl();
